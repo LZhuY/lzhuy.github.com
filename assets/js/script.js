3656d33ba6d9b79e3b9b23203bf5a85d96dd7892
LZhuY <longzhuyi@gmail.com>
2018-05-11 18:47:55 +0800

    change config
    change config

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,11 +12,11 @@ if ($(window).width() <= 1280) {
 
 // Variables
     tag1       = $('.pl__all'),
-    tag2       = $('.place_your_tag_name_here'),
-    tag3       = $('.place_your_tag_name_here'),
-    tag4       = $('.place_your_tag_name_here'),
-    tag5       = $('.place_your_tag_name_here'),
-    tag6       = $('.place_your_tag_name_here');
+    tag2       = $('.数据结构'),
+    tag3       = $('.算法'),
+    tag4       = $('.计算机原理'),
+    tag5       = $('.网络编程'),
+    tag6       = $('.分布式');
 var sidebar    = $('#sidebar'),
     container  = $('#post'),
     content    = $('#pjax'),
